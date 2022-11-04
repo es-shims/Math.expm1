a9812381de54c920bde3d55fd6e9cb81fbba52fa
Jordan Harband <ljharb@gmail.com>
2022-11-04 00:02:42 -0700

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `aud`, `auto-changelog`, `functions-have-names`, `tape`

--- polyfill.js
+++ polyfill.js
@@ -3,13 +3,13 @@
 var implementation = require('./implementation');
 
 module.exports = function getPolyfill() {
-	var native = Math.expm1;
-	if (!native
+	var original = Math.expm1;
+	if (!original
 		// FF 35 on Linux reports 22025.465794806725 for Math.expm1(10)
-		|| native(10) !== 22025.465794806718 // eslint-disable-line no-magic-numbers
+		|| original(10) !== 22025.465794806718 // eslint-disable-line no-magic-numbers
 	) {
 		return implementation;
 	}
 
-	return native;
+	return original;
 };
